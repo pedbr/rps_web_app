061b696b9528c37c8a19184e01c84ea3bf359a27
pedro_bras <pedro.c.bras@gmail.com>
2019-07-11 21:47:44 +0200

    adds functions to all buttons with console logging different choices

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
-const playGame = () => {
-    console.log('Button was clicked!')
+let playRock = () => {
+    console.log('You chose Rock!')
 }
+
+let playPaper = () => {
+    console.log('You chose Paper!')
+}
+
+let playScissors = () => {
+    console.log('You chose Scissors!')
+}
+
+document.getElementById("play-rock").addEventListener("click", playRock);
+document.getElementById("play-paper").addEventListener("click", playPaper);
+document.getElementById("play-scissors").addEventListener("click", playScissors);
